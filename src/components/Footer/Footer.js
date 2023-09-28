12ca07662040164c54989fc4dcaf4d71348e9af8
thonino <thoninoben@gmail.com>
2023-09-28 17:14:03 +0200

    maj

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => {
     <footer className="bg-dark text-white-50 text-center py-1 mt-3 fixed-bottom">
       <span className="d-block d-sm-inline">&copy; 2023 contact@maboulangerie.fr </span>
       
-        <Link className="text-light text-decoration-none me-1" to="/legalMentions">
+        <Link className=" fst-italic text-orange me-1" to="/legalMentions">
           Mentions légales
         </Link>
-        <Link className="text-light text-decoration-none" to="/Confidentiality">
+        <Link className=" fst-italic text-orange" to="/Confidentiality">
           Politique de confidentialité
         </Link>
-        <Link className="text-light me-1 text-decoration-none" to="/cgv">
+        <Link className=" me-1 fst-italic text-orange" to="/cgv">
           {' '}CGV
         </Link>
       
