2a9d52c336b2a4a2f15ecced5be9a10383fd34fe
thonino <thoninoben@gmail.com>
2023-10-09 16:57:08 +0200

    presque

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -21,7 +21,7 @@ const Basket = () => {
   // Récupérer les éléments du panier depuis l'API
   const fetchBasket = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/basket", {
+      const response = await axios.get("https://uppercase-back-1eec3e8a2cf1.herokuapp.com/basket", {
         withCredentials: true,
       });
       setCartItems(response.data.cartItems);
@@ -72,7 +72,7 @@ const Basket = () => {
   const updateQuantities = async (quantities) => {
     try {
       const response = await axios.post(
-        "http://localhost:5000/update-quantities",
+        "https://uppercase-back-1eec3e8a2cf1.herokuapp.com/update-quantities",
         quantities,
         {
           withCredentials: true,
@@ -96,7 +96,7 @@ const Basket = () => {
     e.preventDefault();
     try {
       const response = await axios.get(
-        `http://localhost:5000/removeProduct/${productId}`,
+        `https://uppercase-back-1eec3e8a2cf1.herokuapp.com/removeProduct/${productId}`,
         {
           withCredentials: true,
         }
@@ -114,7 +114,7 @@ const Basket = () => {
   const clearBasket = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.get("http://localhost:5000/clearBasket", {
+      const response = await axios.get("https://uppercase-back-1eec3e8a2cf1.herokuapp.com/clearBasket", {
         withCredentials: true,
       });
       if (response.data.success) {
@@ -136,7 +136,7 @@ const Basket = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/validateBasket",
+        "https://uppercase-back-1eec3e8a2cf1.herokuapp.com/validateBasket",
         data,
         {
           withCredentials: true,
