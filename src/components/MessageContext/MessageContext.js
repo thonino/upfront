1f89f339884e8387a6db2d74a10df45da69b12c8
thonino <thoninoben@gmail.com>
2023-11-09 13:29:35 +0100

    bug user message

--- src/components/MessageContext/MessageContext.js
+++ src/components/MessageContext/MessageContext.js
@@ -25,7 +25,7 @@ export const MessageProvider = ({ children }) => {
       };
       fetchMessages();
     }
-  }, []); 
+  }, [user]); 
 
   const markMessageAsRead = async (messageId) => {
     try {
