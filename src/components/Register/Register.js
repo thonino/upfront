9bc1302153971156255a292419cbe8b92a7f718c
thonino <thoninoben@gmail.com>
2023-11-17 09:54:23 +0100

    mdp register secure

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,6 +5,7 @@ const Register = () => {
   const [prenom, setPrenom] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const navigate = useNavigate();
 
@@ -16,38 +17,49 @@ const Register = () => {
   };
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+    // Validation du mot de passe
+    if (e.target.value.length < 10) {
+      setPasswordError("Le mot de passe doit contenir au moins 10 caractères");
+    } else {
+      setPasswordError("");
+    }
   };
   const handleSubmit = (e) => {
     e.preventDefault();
+    // Vérifier si le mot de passe est valide
+    if (password.length < 10) {
+      setPasswordError("Le mot de passe doit contenir au moins 10 caractères");
+      return;
+    }
+
     const data = {
       email: email,
       prenom: prenom,
       password: password,
-      role: "client"
+      role: "client",
     };
-  
+
     fetch("https://uppercase-app-back-efd9a0ca1970.herokuapp.com/register/new", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         navigate("/login");
       })
       .catch((error) => {
         console.error("Erreur lors de l'envoi :", error);
       });
   };
-  
 
   return (
     <div className="container col-6">
       <h1 className="text-center c1 pacifico">S'inscrire</h1>
       <form onSubmit={handleSubmit} encType="multipart/form-data">
-      <div className="mb-3">
+        <div className="mb-3">
           <label htmlFor="Prenom" className="fst-italic form-label roboto">
             Prenom
           </label>
@@ -71,9 +83,12 @@ const Register = () => {
             onChange={handleEmailChange}
           />
         </div>
-        
+
         <div className="mb-3">
-          <label htmlFor="Password" className="fst-italic form-label roboto">
+          <label
+            htmlFor="Password"
+            className="fst-italic form-label roboto"
+          >
             Password
           </label>
           <input
@@ -83,6 +98,9 @@ const Register = () => {
             value={password}
             onChange={handlePasswordChange}
           />
+          {passwordError && (
+            <p className="text-danger">{passwordError}</p>
+          )}
         </div>
         <button type="submit" className="bouton-1">
           Enregistrer
@@ -93,5 +111,3 @@ const Register = () => {
 };
 
 export default Register;
-
-
